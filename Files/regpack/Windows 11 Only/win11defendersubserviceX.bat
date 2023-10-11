b0270d96052295781131138b14a7520e358fe9a6
Viktor Antonov <vijorich@mail.ru>
2023-10-11 20:53:02 +0700

    RegPack update 1.53 > 1.54

--- Files/regpack/Windows 11 Only/win11defendersubserviceX.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-for /f "delims=Endofsearch: " %%r in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /f webthreatdefusersvc_ /k') do if %%r==0 ( echo ERROR: Windows 11 Defender Sub-Service was not found. ) else ( for /f "eol=E" %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /f webthreatdefusersvc_ /k') do reg add %%i /v Start /t REG_DWORD /d 4 /f )
-pause
